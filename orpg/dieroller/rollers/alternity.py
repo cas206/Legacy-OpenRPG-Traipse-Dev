1660f8b47df34a2ab298f6cda8cf60654a555b32
Tyler Starke <sirebral@unknown.com>
2010-01-25 10:03:16 -0600

    Traipse Beta 'OpenRPG' {100125-00}
    Traipse is a distribution of OpenRPG that is designed to be easy to
    setup and go. Traipse also makes it easy for developers to work on code
    without fear of sacrifice. 'Ornery-Orc' continues the trend of 'Grumpy'
    and adds fixes to the code. 'Ornery-Orc's main goal is to offer more
    advanced features and enhance the productivity of the user.
    
    Update Summary (Beta)
    New Features:
    Added Bookmarks
    Added 'boot' command to remote admin
    Added confirmation window for sent nodes
    Minor changes to allow for portability to an OpenSUSE linux OS
    Miniatures Layer pop up box allows users to turn off Mini labels, from
    FlexiRPG
    Zoom Mouse plugin added
    Images added to Plugin UI
    Switching to Element Tree
    Map efficiency, from FlexiRPG
    Added Status Bar to Update Manager
    New TrueDebug Class in orpg_log (See documentation for usage)
    Portable Mercurial
    Tip of the Day added, from Core and community
    New Reference Syntax added for custom PC sheets
    New Child Reference for gametree
    New Parent Reference for gametree
    New Gametree Recursion method, mapping, context sensitivity, and
    effeciency..
    New Features node with bonus nodes and Node Referencing help added
    Dieroller structure from Core
    New DieRoller portability for odd Dice
    Added 7th Sea die roller; ie [7k3] = [7d10.takeHighest(3).open(10)]
    New 'Mythos' System die roller added
    Added new vs. die roller method for WoD; ie [3v3] = [3d10.vs(3)].
    Included for Mythos roller also
    New Warhammer FRPG Die Roller (Special thanks to Puu-san for the
    support)
    New EZ_Tree Reference system. Push a button, Traipse the tree, get a
    reference (Beta!)
    
    Fixes:
    Fix to Text based Server
    Fix to Remote Admin Commands
    Fix to Pretty Print, from Core
    Fix to Splitter Nodes not being created
    Fix to massive amounts of images loading, from Core
    Fix to Map from gametree not showing to all clients
    Fix to gametree about menus
    Fix to Password Manager check on startup
    Fix to PC Sheets from tool nodes. They now use the tabber_panel
    Fix to Whiteboard ID to prevent random line or text deleting.
    Fixes to Server, Remote Server, and Server GUI
    Fix to Update Manager; cleaner clode for saved repositories
    Fixes made to Settings Panel and now reactive settings when Ok is
    pressed
    Fixes to Alternity roller's attack roll. Uses a simple Tuple instead of
    a Splice
    Fix to Use panel of Forms and Tabbers. Now longer enters design mode
    Fix made Image Fetching. New fetching image and new failed image
    Modified ID's to prevent non updated clients from ruining the fix.
    default_manifest.xml renamed to default_upmana.xml

--- orpg/dieroller/rollers/alternity.py
+++ orpg/dieroller/rollers/alternity.py
@@ -25,9 +25,12 @@
 # between the source and the intrepretor. IF, ELIF statements have been replaced with dictionaries,
 # unused objects have been replace with re-usable objects, and the code has been condensed.
 #
-# SEG: JAN 21 2010 - v.1.2 O'Flux Release:
+# SEG: JAN 24 2010 - v.1.4 O'Flux Release:
 # Edits & Additions: fixed a few minor bugs; Damage roll & Display Issues.
 # Added Secondary Damage Calculation and Display. Fix all errors.
+# Added proper results for Critcal Successes with failure ==> final Result Ordinary Success
+# Removed reduntent Method to make parent class true with all others working as child.
+# Made all special output same colour codes font size. Cleaned out old commented lines.
 # Tested for Traipse on Win 7
 #
 #  Skill Check Example:
@@ -55,7 +58,7 @@ from std import std
 from time import time, clock
 from orpg.dieroller.base import di, die_base, die_rollers
 
-## from orpg.tools.orpg_log import debug
+##from orpg.tools.orpg_log import debug
 
 
 __version__ = "$Id: alternity.py,v 0.1 2003/01/02 12:00:00 cchriss Exp $"
@@ -100,13 +103,17 @@ class sk(std):
     def getRolLStr(self):
         myStr = "[" + str(self.data[0])
         self.d20 = self.sum()
-        amod = self.getMod(self.mod)
-        self.dieRoll = self.d20 + amod
+        self.amod = self.getMod(self.mod)
+
+##        varN = "self.amod"
+##        debug(varN)
+##        debug(self.amod)  ## seg added debug output
+        
+        self.dieRoll = self.d20 + self.amod
         for a in self.data[1:]:
             myStr += ","
             myStr += str(a)
-        myStr += "," + str(amod) + "] = (" + str(self.dieRoll) + ")"
-##        if ( self.d20 == 1 ): self.success = 'CS' # seg - removed - unneeded ** #
+        myStr += "," + str(self.amod) + "] = (" + str(self.dieRoll) + ")"
         if ( self.dieRoll <= self.score / 4 ): self.success = 'A'
         elif ( self.dieRoll <= self.score / 2 ): self.success = 'G'
         elif ( self.dieRoll <= self.score ): self.success = 'O'
@@ -116,14 +123,23 @@ class sk(std):
 
     def __str__(self):
         myStr = self.getRolLStr()
-        successes = {'CS': " <b><font color='#00aa00'>CRITICAL SUCCESS</font></b>",
-                    'CF': " <b><font color='#ff0000'>CRITICAL FAILURE</font></b>",
-                    'A': " <b><font color='#00aa00'>AMAZING Success</b>",
-                    'G': " <b>Good Success</b>",
-                    'O': " <b>Ordinary Success</b>",
-                    'F': " <b>failure</b>"}
+
+##        varN = "myStr"
+##        debug(varN)
+##        debug(myStr)  ## seg added debug output
+
+        successes = {'CS': " <b><font size=2 color='#8D38C9'>CRITICAL SUCCESS</font></b>",
+                    'CF': " <b><font size=2 color='#151B54'>CRITICAL FAILURE</font></b>",
+                    'A': " <b><font size=2 color='#E42217'>AMAZING Success</font></b>",
+                    'G': " <b><font size=2 color='#306EFF'>Good Success</font></b>",
+                    'O': " <b><font size=2 color='#52D017'>Ordinary Success</font></b>",
+                    'F': " <b><font size=2 color='#41627E'>failure</font></b>"}
+
         if ( self.d20 == 1 ):  myStr += successes['CS'] # SEG Dec 19 2009
         myStr += successes[self.success]
+        if ( self.d20 == 1 ) and (self.success == 'F') :
+            myStr += "          final result ==> "
+            myStr += successes['O'] # SEG JAN 23 2010
         return myStr
 
 class at(sk):
@@ -146,7 +162,11 @@ class at(sk):
         droll = str(dmgroll[0])
         xyz = droll.split('(')
         secD = (int(xyz[1][:-1])/2)   ## SEG* Calculate Secondary Damage
-##  debug(secD)  ## seg added debug output
+
+##        varN = "secD"
+##        debug(varN)
+##        debug(secD)  ## seg added debug output
+
         dtype = dmgroll[1]
         astr += droll
         if dtype=="s": astr += " <b><font size=2 color='#52D017'>stun</font></b><BR>"
@@ -160,13 +180,25 @@ class at(sk):
 
     def __str__(self):
         myStr = self.getRolLStr()
+
+##        varN = "myStr"
+##        debug(varN)
+##        debug(myStr)  ## seg added debug output
+        
         successes = {'CS': " <b><font size=2 color='#8D38C9'>CRITICAL SUCCESS</font></b>",
                     'CF': " <b><font size=2 color='#151B54'>CRITICAL FAILURE</font></b>",
                     'A': " <b><font size=2 color='#E42217'>AMAZING HIT</font></b><BR> ",
                     'G': " <b><font size=2 color='#306EFF'>Good HIT</font></b><BR> ",
                     'O': " <b><font size=2 color='#52D017'>Ordinary HIT</font></b><BR> ",
                     'F': " <b><font size=2 color='#41627E'>miss</font></b>"}
-        if ( self.d20 == 1 ):  myStr += successes['CS'] # SEG Dec 19 2009 
+        if ( self.d20 == 1 ):
+            myStr += successes['CS'] # SEG Dec 19 2009
+
+        if ( self.d20 == 1 ) and (self.success == 'F') :
+            myStr += successes['F'] # SEG JAN 23 2010
+            myStr += "          final result ==> "
+            self.success = 'O'
+
         myStr += successes[self.success]
         if self.success == 'A': myStr += self.getdmg(self.dmga)
         elif self.success == 'G': myStr += self.getdmg(self.dmgg)
@@ -177,33 +209,28 @@ class ac(sk):
     def __init__(self,source=[],sc=10,mod=0):
         sk.__init__(self,source,sc,mod)
 
-    def GetRoLLStr(self):
-        myStr = "[" + str(self.data[0])
-        self.d20 = self.sum()
-        amod = self.getMod(self.mod)
-        self.dieRoll = self.d20 + amod
-        for a in self.data[1:]:
-            myStr += ","
-            myStr += str(a)
-        myStr += "," + str(amod) + "] = (" + str(self.dieRoll) + ")"
-        if ( self.dieRoll <= self.score / 4 ): self.success = 'A'
-        elif ( self.dieRoll <= self.score / 2 ): self.success = 'G'
-        elif ( self.dieRoll <= self.score ): self.success = 'O'
-        else: self.success = 'F'
-        if ( self.d20 == 20 ): self.success = 'CF'
-        return myStr
-
     def __str__(self):
-        myStr = self.GetRoLLStr()
+        myStr = self.getRolLStr()
+
+##        varN = "myStr"
+##        debug(varN)
+##        debug(myStr)  ## seg added debug output
+        
         myStr = " <b><font color='#E42217'>ACTION CHECK : </font></b>"+myStr
-        successes = {'CS': " <b><font color='#00aa00'>CRITICAL SUCCESS</font></b>",
-                    'CF': " <b><font color='#ff0000'>CRITICAL FAILURE</font></b><BR> -2 Step make up bonus next Action Check",
-                    'A': " <b><font color='#00aa00'>AMAZING Success</b>",
-                    'G': " <b>Good Success</b>",
-                    'O': " <b>Ordinary Success</b>",
-                    'F': " <b>Marginal failure</b><BR> -1 Step make up bonus next Action Check"}
+        successes = {'CS': " <b><font size=2 color='#8D38C9'>CRITICAL SUCCESS</font></b>",
+                    'CF': " <b><font size=2 color='#151B54'>CRITICAL FAILURE</font></b><BR> -2 Step make up bonus next Action Check",
+                    'A': " <b><font size=2 color='#E42217'>AMAZING Success</font></b>",
+                    'G': " <b><font size=2 color='#306EFF'>Good Success</font></b>",
+                    'O': " <b><font size=2 color='#52D017'>Ordinary Success</font></b>",
+                    'F': " <b><font size=2 color='#41627E'>Marginal failure</font></b>"}
         if ( self.d20 == 1 ):  myStr += successes['CS'] # SEG Dec 19 2009
         myStr += successes[self.success]
+        if ( self.d20 == 1 ) and (self.success == 'F') :
+            myStr += " final result ==> "
+            myStr += successes['O'] # SEG JAN 23 2010
+        if ( self.d20 != 1 ) and (self.success == 'F') :
+            myStr += "<BR> -1 Step make up bonus next Action Check"
+            
         return myStr
 
 
